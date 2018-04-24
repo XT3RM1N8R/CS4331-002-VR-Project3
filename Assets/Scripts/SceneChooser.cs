fac3dedae35ce3f5b38de02419e9ebc6d4f9f71f
Darien Sokolov <darien.sokolov@yahoo.com>
2018-04-23 21:34:27 -0500

    Copied some old assets

---
+++ Assets/Scripts/SceneChooser.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChooser : MonoBehaviour {
+
+	public GameObject[] objects;
+
+	public void SwitchSceneToDenver(){
+		SceneManager.LoadScene("DenverScene", LoadSceneMode.Single);
+
+	}
+
+	public void SwitchScenetoEllsworth(){
+		SceneManager.LoadScene ("EllsworthScene", LoadSceneMode.Single);
+	}
+
+	public void SwitchScenetoLubbock(){
+		SceneManager.LoadScene ("Lubbock", LoadSceneMode.Single);
+	}
+
+	public void ToInstruction(){
+		objects [0].SetActive (false);
+		objects [1].SetActive (true);
+	}
+
+	public void Back(){
+		objects [0].SetActive (true);
+		objects [1].SetActive (false);
+	}
+}
---
+++ Assets/Scripts/TeleportTerrain.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportTerrain : MonoBehaviour {
+	public GameObject[] objects;
+  public GameObject waterPlane;
+	public void teleportLubbock(){
+    startScene();
+		var startingcam = GameObject.Find ("StartingCam");
+		startingcam.SetActive (false);
+
+		objects [0].SetActive (true);
+		objects[0].transform.position = new Vector3(18,53,27);
+		Screen.lockCursor = true;
+
+		var playerevent = GameObject.Find("TeleportPlayer");
+		playerevent.SetActive (false);
+
+		var canvas = GameObject.Find("Canvas");
+		canvas.SetActive (false);
+
+	}
+
+
+	public void teleportDenver() {
+    startScene();
+    var startingcam = GameObject.Find ("StartingCam");
+		startingcam.SetActive (false);
+
+		objects [0].SetActive (true);
+		objects[0].transform.position = new Vector3(51, 156, 53);
+		Screen.lockCursor = true;
+
+		var playerevent = GameObject.Find("TeleportPlayer");
+		playerevent.SetActive (false);
+
+		var canvas = GameObject.Find("Canvas");
+		canvas.SetActive (false);
+
+	}
+
+	public void teleportEllsworth() {
+    startScene();
+    var startingcam = GameObject.Find ("StartingCam");
+		startingcam.SetActive (false);
+
+		objects [0].SetActive (true);
+		objects[0].transform.position = new Vector3(18,6, 36);
+		Screen.lockCursor = true;
+
+		var playerevent = GameObject.Find("TeleportPlayer");
+		playerevent.SetActive (false);
+
+		var canvas = GameObject.Find("Canvas");
+		canvas.SetActive (false);
+
+	}
+
+  public void startScene() {
+    waterPlane.SetActive(true);
+    CamChange.sceneStarted = true;
+  }
+}
+	
