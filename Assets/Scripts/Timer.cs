52bb109efbee2926d8cd8943377d0669e952dd17
JuJoCoe <jordan.j.coe@gmail.com>
2018-04-25 11:42:47 -0500

    Score and Timer Bar

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour
+{
+	float timeLeft = 120.0f;
+
+	public Text text;
+
+
+
+	void Update()
+	{
+		timeLeft -= Time.deltaTime;
+		text.text = "Time Left:" + Mathf.Round(timeLeft);
+		if(timeLeft < 0)
+		{
+			Application.LoadLevel("gameOver");
+		}
+	}
+}
