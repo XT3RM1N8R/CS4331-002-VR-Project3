110607c5c2f1615a91ce450442fb68d9e59eb2ef
Darien Sokolov <darien.sokolov@yahoo.com>
2018-05-05 21:11:44 -0500

    Fire/Water Interactions Enabled
    DeathByWater script allows fire assets to have a health system with
    damage by water. Water layer allows for water detection by fire.
    Colliders added to fire for water particle collision detection.
    
    Fire assets resistance can be configured via the DeathByWater script.
    
    Some fire asset prefabs had to be reverted and modified in the scene in
    order to restore proper prefab binding and allow the colliders to
    properly represent the bounds of the fire.
    
    Water spray toggle enabled with primary (Left) mouse button held-down.
    
    Water spray particle system configured.
    
    These interactions are in a rudimentary form and are subject to change
    and tuning.
    
    FireLayer simply fixes a bug in the scripts that came with the fire
    assets.
    
    Custom Fluvio Particle - FireFrenzy is a custom water material shader
    for this scene.

---
+++ Assets/DeathByWater.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathByWater : MonoBehaviour {
+  public int endurance = 10000;
+  public int resistance = 10;
+  public int damage = 1000;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+  // Update is called once per frame
+  void Update() {
+    if (endurance <= 0) {
+      Destroy(this.gameObject);
+    }
+  }
+
+  void OnParticleCollision(GameObject other) {
+    print("Collision");
+    if (other.tag == "Water") {
+      endurance -= damage / resistance;
+      print("Fire being extinguished");
+    }
+  }
+}
---
+++ Assets/HoseToggle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoseToggle : MonoBehaviour {
+  private ParticleSystem fluidParticles;
+  [SerializeField]
+  private bool sprayEnabled;
+	// Use this for initialization
+	void Start () {
+    fluidParticles = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    print("We have this component: " + fluidParticles);
+    if (!sprayEnabled) {
+      if (Input.GetMouseButton(0)) {
+        print("Enabled Spray");
+        sprayEnabled = true;
+        //fluidParticles.emission.enabled = true;
+        fluidParticles.Play();
+        fluidParticles.enableEmission = true;
+      }
+    } else {
+      if (!Input.GetMouseButton(0)) {
+        print("Disabled Spray");
+        sprayEnabled = false;
+        //fluidParticles.emission.enabled = true;
+        fluidParticles.enableEmission = false;
+        fluidParticles.Stop();
+      }
+    }
+	}
+}
