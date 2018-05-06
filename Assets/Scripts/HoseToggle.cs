870a4e5d8e7bf835ab64c45dd9e7975ac1c84122
JuJoCoe <jordan.j.coe@gmail.com>
2018-05-05 23:54:35 -0500

    Sound for water

--- Assets/Scripts/HoseToggle.cs
+++ Assets/Scripts/HoseToggle.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class HoseToggle : MonoBehaviour {
   private ParticleSystem fluidParticles;
+	private AudioSource audio;
   [SerializeField]
   private bool sprayEnabled;
 	// Use this for initialization
 	void Start () {
     fluidParticles = GetComponent<ParticleSystem>();
+		audio = GetComponent<AudioSource> ();
+		audio.volume = 0.0f;
 	}
 	
 	// Update is called once per frame
@@ -18,6 +21,7 @@ public class HoseToggle : MonoBehaviour {
       if (Input.GetMouseButton(0)) {
         print("Enabled Spray");
         sprayEnabled = true;
+		audio.volume = 1.0f;
         //fluidParticles.emission.enabled = true;
         fluidParticles.Play();
         fluidParticles.enableEmission = true;
@@ -26,6 +30,7 @@ public class HoseToggle : MonoBehaviour {
       if (!Input.GetMouseButton(0)) {
         print("Disabled Spray");
         sprayEnabled = false;
+		audio.volume = 0.0f;
         //fluidParticles.emission.enabled = true;
         fluidParticles.enableEmission = false;
         fluidParticles.Stop();
