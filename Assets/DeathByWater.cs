7b7bad07f99f4fa65393eb8d6b3e39e75f636791
Darien Sokolov <darien.sokolov@yahoo.com>
2018-05-05 22:45:21 -0500

    Fire now disables child elements properly

--- Assets/DeathByWater.cs
+++ Assets/DeathByWater.cs
@@ -6,6 +6,7 @@ public class DeathByWater : MonoBehaviour {
   public int endurance = 10000;
   public int resistance = 10;
   public int damage = 1000;
+  private bool isAlive = true;
 	// Use this for initialization
 	void Start () {
 		
@@ -13,16 +14,24 @@ public class DeathByWater : MonoBehaviour {
 
   // Update is called once per frame
   void Update() {
-    if (endurance <= 0) {
-      Destroy(this.gameObject);
+    if (endurance <= 0 && isAlive) {
+      isAlive = false;
+      print("Fire is Extinguished");
+      foreach(Transform child in this.transform) {
+        ParticleSystem particleSystem = child.gameObject.GetComponent<ParticleSystem>();
+        if(particleSystem) {
+          particleSystem.enableEmission = false;
+          particleSystem.Stop();
+        } else {
+          child.gameObject.SetActive(false);
+        }
+      }
     }
   }
 
   void OnParticleCollision(GameObject other) {
-    print("Collision");
     if (other.tag == "Water") {
       endurance -= damage / resistance;
-      print("Fire being extinguished");
     }
   }
 }
